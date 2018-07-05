b92f6298185c152b4832773c7bc958e808e20ebb
Jakob Brobak <jakobbrobak@gmail.com>
2018-07-05 12:05:56 +0200

    Opdatering

--- gallery/admin/includes/database.php
+++ gallery/admin/includes/database.php
@@ -5,7 +5,13 @@ require_once("new_config.php");
 
 class Database {
 
-    private $connection;
+    public $connection;
+
+    function __construct() {
+
+        $this->open_db_connection();
+
+    }
 
     public function open_db_connection() {
        
@@ -19,11 +25,34 @@ class Database {
 
     }
 
+    public function query($sql) {
+
+        $result = mysqli_query($this->connection, $sql);
+        
+        return $result;
+
+    }
+
+    private function confirm_query($result) {
+
+        if(!result) {
+            die("Query Failed");
+        }       
+
+    }
+
+    public function escape_string($string) {
+
+        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+
+        return $escaped_string; 
+
+    }
+
 }
 
 $database = new Database();
 
-$database->open_db_connection();
 
 
 
