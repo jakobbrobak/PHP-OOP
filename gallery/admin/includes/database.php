6eb2c94d1271e5b1db3a9cdb78024080f48e8fa7
Jakob Brobak <jakobbrobak@gmail.com>
2018-07-05 15:23:43 +0200

    Opdatering

--- gallery/admin/includes/database.php
+++ gallery/admin/includes/database.php
@@ -15,11 +15,11 @@ class Database {
 
     public function open_db_connection() {
        
-        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-        if(mysqli_connect_error()) {
+        if($this->connection->connect_errno) {
 
-            die("Database connection couldnt be established!" . mysqli_error());
+            die("Database connection couldnt be established!" . $this->connection->connect_error);
 
         }
 
@@ -27,7 +27,9 @@ class Database {
 
     public function query($sql) {
 
-        $result = mysqli_query($this->connection, $sql);
+        $result = $this->connection->query($sql);
+
+        $this->confirm_query($result);
         
         return $result;
 
@@ -36,16 +38,22 @@ class Database {
     private function confirm_query($result) {
 
         if(!result) {
-            die("Query Failed");
+            die("Query Failed") . $this->connection->error;
         }       
 
     }
 
     public function escape_string($string) {
 
-        $escaped_string = mysqli_real_escape_string($this->connection, $string);
+        $escaped_string = $this->connection->real_escape_string($string);
 
         return $escaped_string; 
+
+    }
+
+    public function the_insert_id() {
+
+        return $this->connection->insert_id;
 
     }
 
