cd218351dc0f12722d2a054837ebdc8d971809c7
Jakob Brobak <jakobbrobak@gmail.com>
2018-07-05 11:30:36 +0200

    Opdateringer

--- gallery/admin/includes/side_nav.php
+++ gallery/admin/includes/side_nav.php
@@ -4,13 +4,16 @@
                         <a href="index.php"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
                     </li>
                     <li>
-                        <a href="users.php"><i class="fa fa-fw fa-bar-chart-o"></i> Users</a>
+                        <a href="users.php"><i class="fa fa-fw fa-user"></i> Users</a>
                     </li>
                     <li>
-                        <a href="upload.php"><i class="fa fa-fw fa-table"></i> Upload</a>
+                        <a href="upload.php"><i class="fa fa-fw fa-upload"></i> Upload</a>
                     </li>
                     <li>
-                        <a href="comments.php"><i class="fa fa-fw fa-edit"></i> Comments</a>
+                        <a href="photos.php"><i class="fa fa-fw fa-photo"></i> Photos</a>
+                    </li>
+                    <li>
+                        <a href="comments.php"><i class="fa fa-fw fa-comment"></i> Comments</a>
                     </li>
                 </ul>
             </div>
