452d9d81d44a629d8fbef145c39e6b6b8d81c1a8
Jakob Brobak <jakobbrobak@gmail.com>
2018-07-10 10:43:51 +0200

    Opdatering

--- gallery/admin/includes/user.php
+++ gallery/admin/includes/user.php
@@ -36,7 +36,7 @@ class User {
 
     }
 
-    public static function instantiation($user_found) {
+    public static function instantiation($the_record) {
 
         $the_object = new self;
 
@@ -46,8 +46,26 @@ class User {
         $the_object->first_name = $user_found['first_name']; 
         $the_object->last_name = $user_found['last_name']; 
 
+        foreach ($the_record as $the_attribute => $value) {
+
+            if($the_object->has_the_attribute($the_attribute)) {
+
+                   $the_object->$the_attribute = $value; 
+            }
+
+        }
+
         return $the_object;
-    }
+
+       }
+
+       private function has_the_attribute($the_attribute) {
+
+           $object_properties = get_object_vars($this);
+
+           return array_key_exists($the_attribute, $object_properties); 
+
+       }
 
 
 }
