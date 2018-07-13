8aec4c22147b6d186f4dad672b3adf072c86a84b
Jakob Brobak <jakobbrobak@gmail.com>
2018-07-13 10:42:35 +0200

    Opdatering

--- gallery/admin/includes/init.php
+++ gallery/admin/includes/init.php
@@ -4,7 +4,7 @@ require_once("new_config.php");
 require_once("database.php");
 require_once("user.php");
 require_once("functions.php");
-require_once("sessions.php");
+require_once("session.php");
 
 
 
