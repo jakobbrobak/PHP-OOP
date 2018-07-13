a2cdd8dac2217c8626c0b9f6621cab3fe1ce4c5e
Jakob Brobak <jakobbrobak@gmail.com>
2018-07-13 11:59:18 +0200

    Opdatering

---
+++ basic files/file.php
@@ -0,0 +1,41 @@
+<?php 
+
+echo __FILE__ . "<br>";
+
+echo __LINE__ . "<br>";
+
+echo __DIR__ . "<br>";
+
+if(file_exists(__DIR__)) {
+
+    echo "Yes" . "<br>";
+}
+
+if(is_file(__DIR__)) {
+
+    echo "Yes" . "<br>";
+} else {
+
+    echo "No" . "<br>";
+}
+
+if(is_file(__FILE__)) {
+
+    echo "Yes" . "<br>";
+} else {
+
+    echo "No" . "<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
