6a79041a5818b8095161a123cf4291b34b3befa3
Jakob Brobak <jakobbrobak@gmail.com>
2018-07-18 15:30:59 +0200

    Opdatering

--- gallery/admin/includes/user.php
+++ gallery/admin/includes/user.php
@@ -1,7 +1,7 @@
 <?php 
 
 
-class User {
+class User extends Db_object {
 
     protected static $db_table = "users";
     protected static $db_table_fields = array('username', 'password', 'first_name', 'last_name');
@@ -10,41 +10,7 @@ class User {
     public $password;
     public $first_name;
     public $last_name;
-
-   
-    public static function find_all_users() {
-
-      return self::find_this_query("SELECT * FROM users");
-
-
-    }
-
-    public static function find_user_by_id($user_id) {
-
-        $the_result_array = self::find_this_query("SELECT * FROM users WHERE id = $user_id LIMIT 1");
-
-        return !empty($the_result_array) ? array_shift($the_result_array) : false;
-
-    }
           
-
-    public static function find_this_query($sql) {
-        
-        global $database;
-
-        $result_set = $database->query($sql);
-        $the_object_array = array();
-
-        while($row = mysqli_fetch_array($result_set)) {
-            
-            $the_object_array[] = self::instantiation($row);
-
-        }
-
-        return $the_object_array;
-       
-
-    }
 
     public static function verify_user($username, $password) {
 
@@ -53,7 +19,7 @@ class User {
         $username = $database->escape_string($username);
         $password = $database->escape_string($password);
 
-        $sql = "SELECT * FROM users WHERE ";
+        $sql = "SELECT * FROM " .  self::$db_table .  " WHERE ";
         $sql .= "username = '{$username}' ";
         $sql .= "AND password = '{$password}' "; 
         $sql .= "LIMIT 1";
@@ -64,31 +30,6 @@ class User {
         
 
     }
-
-    public static function instantiation($the_record) {
-
-        $the_object = new self;
-
-        foreach ($the_record as $the_attribute => $value) {
-
-            if($the_object->has_the_attribute($the_attribute)) {
-
-                   $the_object->$the_attribute = $value; 
-            }
-
-        }
-
-        return $the_object;
-
-       }
-
-       private function has_the_attribute($the_attribute) {
-
-           $object_properties = get_object_vars($this);
-
-           return array_key_exists($the_attribute, $object_properties); 
-
-       }
 
        protected function properties() {
 
@@ -152,13 +93,9 @@ class User {
 
             return false;
 
-        }
-           
-
+        }    
 
        }
-
-
 
        public function update() {
 
